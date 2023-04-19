c8e532d37a355fc4d4d554fe12c07c11d3b9f44b
Lucas <lucaszhengrui@gmail.com>
2023-04-20 00:30:44 +0100

    Add database code

--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,40 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
+# Data details:
+# Uniq Id,Product Name,Brand Name,Category,Selling Price,Model Number,
+# About Product,Product Specification,Technical Details,Shipping Weight,Product Dimensions,Image,
+# Variants,Sku,Product Url,Stock,Product Details,Dimensions,Color,Ingredients,Direction To Use,Is Amazon Seller
 
 # Create your models here.
+class basicinfo(models.Model):
+    Unique_id = models.TextField()
+    Product_name = models.TextField()
+    Brand_name = models.TextField()
+    Category = models.TextField()
+    Selling_price = models.TextField()
+    Image = models.TextField()
+    Amazon_seller = models.TextField()
+
+    def __str__(self):
+        return f'{self.Unique_id}, {self.Product_name}, {self.Brand_name}, {self.Category}, {self.Selling_price}, {self.Image}, {self.Amazon_seller}'
+
+    def get_absolute_url(self):
+        return reverse('order_list', {'pk': self.pk})
+    
+class detail(models.Model):
+    Unique_id = models.ForeignKey('shopping.basicinfo', on_delete=models.CASCADE, related_name='details')
+    Model_number = models.TextField()
+    About_product = models.TextField()
+    Product_specification = models.TextField()
+    Technical_details = models.TextField()
+    Shipping_weight = models.TextField()
+    Product_dimensions = models.TextField()
+
+    def __str__(self):
+        return f'{self.Unique_id}, {self.Model_number}, {self.About_product}, {self.Product_specification}, {self.Technical_details}, {self.Shipping_weight}, {self.Product_dimensions}'
+
+    def get_absolute_url(self):
+        return reverse('order_list', {'pk': self.pk})
