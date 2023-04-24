000ded5b7e63b12f428ca28eef667d3cc6df5482
Lucas <lucaszhengrui@gmail.com>
2023-04-24 22:50:38 +0100

    Now we can see 8 product horizontally

--- shopping/views.py
+++ shopping/views.py
@@ -20,14 +20,7 @@ def index(request, id):
 
 def product_list(request):
     products = basicinfo.info()
-    paginator = Paginator(products, 10) # show 10 products per page
+    paginator = Paginator(products, 8) # show 8 products per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'shopping/product_list.html', {'page_obj': page_obj})
-
-# def product__by_id(request, Unique_id):
-#     products = detail.shopping_by_id(Unique_id)
-#     paginator = Paginator(products, 1) # show 10 products per page
-#     page_number = request.GET.get('page')
-#     page_obj = paginator.get_page(page_number)
-#     return render(request, 'shopping/product_detail.html', {'page_obj': page_obj})
