c0f8a3346b02f2352b0ac56e912e59b52a49c336
Lucas <lucaszhengrui@gmail.com>
2023-05-05 17:43:37 +0100

    Added search feature but it does not work

--- order/views.py
+++ order/views.py
@@ -10,43 +10,6 @@ import sqlite3
 
 # Order stuff part
 
-def get_product():
-    order = detail.objects.order_by('Unique_id').filter(Is_deleted=0)
-    return order
-
-def index(request):
-    order = get_product()
-
-    # Search
-    search_query = request.GET.get('search_query')
-    search_field = request.GET.get('search_field', 'all')
-    if search_query and search_field != 'all':
-        order = order.filter(**{search_field: search_query})
-
-    # Pagination
-    paginator = Paginator(order, 10)  # Show 10 items per page
-    page = request.GET.get('page', 1)
-
-    try:
-        # Convert page to an integer
-        page = int(page)
-        # Validate that the page is greater than or equal to 1
-        if page <= 0:
-            page = 1
-        # Get the page from the paginator
-        orders = paginator.page(page)
-    except (PageNotAnInteger, EmptyPage):
-        # If page is not an integer or out of range, deliver first page.
-        orders = paginator.page(1)
-
-    # Limit the page range to 3 pages
-    page_range = paginator.get_elided_page_range(page, on_each_side=0, on_ends=1)
-
-    message_obj = get_message_obj()
-
-    return render(request, 'order/product_detail.html', {'order': orders, 'search_query': search_query,
-                                                         'search_field': search_field, 'page_range': page_range, 'message_obj':message_obj})
-
 def order(request, id):
     order = detail.objects.get(id=id) # The 'id' in this place should be the unique one which was created by shopping/model (build database or read csv)
     order.Is_deleted = 1
--- shopping/views.py
+++ shopping/views.py
@@ -27,3 +27,49 @@ def product_list(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'shopping/product_list.html', {'page_obj': page_obj})
+
+def get_product():
+    product = basicinfo.objects.order_by('id')
+    return product
+
+def search(request):
+    product = get_product()
+
+    # Search
+    search_query = request.GET.get('search_query')
+    search_field = request.GET.get('search_field', 'all')
+    print(search_field)
+    print(search_query) 
+    # Check if the search feature work correctly
+    if search_query and search_field != 'all':
+        product = product.filter(**{search_field: search_query})
+
+    connection = sqlite3.connect('db.sqlite3')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM shopping_basicinfo WHERE id = ?', (search_query,))
+    myList = cursor.fetchall()
+    message_obj = get_message_obj()
+    connection.close()
+
+    # Pagination
+    paginator = Paginator(product, 10)  # Show 10 items per page
+    page = request.GET.get('page', 1)
+
+    try:
+        # Convert page to an integer
+        page = int(page)
+        # Validate that the page is greater than or equal to 1
+        if page <= 0:
+            page = 1
+        # Get the page from the paginator
+        products = paginator.page(page)
+    except (PageNotAnInteger, EmptyPage):
+        # If page is not an integer or out of range, deliver first page.
+        products = paginator.page(1)
+
+    # Limit the page range to 3 pages
+    page_range = paginator.get_elided_page_range(page, on_each_side=0, on_ends=1)
+
+    message_obj = get_message_obj()
+
+    return render(request, 'shopping/product_list.html', {'products': myList, 'search_query': search_query, 'search_field': search_field,'page_range': page_range, 'message_obj':message_obj})
